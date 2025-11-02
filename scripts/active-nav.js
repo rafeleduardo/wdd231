a6532852fa986a3c1a128f5469e15df5dac496f5
Rafael Parra <rparra@byupathway.edu>
2025-11-02 01:36:29 +0100

    Added waypoint

---
+++ scripts/active-nav.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const currentPath = window.location.pathname;
+    const navLinks = document.querySelectorAll('#primary-navigation a');
+
+    navLinks.forEach(link => {
+        const linkHref = link.getAttribute('href');
+        let normalizedLinkPath = linkHref.startsWith('/') ? linkHref : '/' + linkHref;
+        let normalizedCurrentPath = currentPath.startsWith('/') ? currentPath : '/' + currentPath;
+
+        if (normalizedCurrentPath === '/' && normalizedLinkPath === '/index.html') {
+            link.classList.add('active');
+        } else if (normalizedCurrentPath.includes(normalizedLinkPath) && normalizedLinkPath !== '/') {
+            link.classList.add('active');
+        }
+    });
+});
